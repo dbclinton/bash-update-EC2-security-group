bf1b55bdfb7c2cb6caa3c55b0e5b4c27573536a2
Taylor Price <tayworm@gmail.com>
2017-03-30 09:15:29 -0700

    dont dump a bunch of files on my system

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# script to pull my current public IP address 
-# and add a rule to my EC2 security group allowing me SSH access 
-curl v4.ifconfig.co > ip.txt
-awk '{ print $0 "/32" }' < ip.txt > ipnew.txt
-export stuff=$ (cat ipnew.txt)
+# script to pull my current public IP address
+# and add a rule to my EC2 security group allowing me SSH access
 aws ec2 authorize-security-group-ingress --group-name NewGroup \
- --protocol tcp --port 22 --cidr $stuff
+ --protocol tcp --port 22 --cidr $(echo "$(curl -s v4.ifconfig.co)/32")
