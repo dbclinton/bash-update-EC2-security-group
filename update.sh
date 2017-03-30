2566b0c60962094887e80e2acc8b6bd870b3afe0
Taylor Price <tayworm@gmail.com>
2017-03-30 09:33:52 -0700

    formatting

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # script to pull my current public IP address
 # and add a rule to my EC2 security group allowing me SSH access
-aws ec2 authorize-security-group-ingress --group-name NewGroup \
- --protocol tcp --port 22 --cidr "$(curl -s v4.ifconfig.co)/32"
+aws ec2 authorize-security-group-ingress \
+--group-name NewGroup \
+ --protocol tcp \
+--port 22 \
+--cidr "$(curl -s v4.ifconfig.co)/32"
