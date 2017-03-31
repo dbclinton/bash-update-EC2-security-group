befa73146efa901dab954796692d16623f719cdf
David Clinton <dbclin@gmail.com>
2017-03-31 10:45:05 -0400

    syntac correction to short version

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 # and add a rule to my EC2 security group allowing me SSH access 
 curl v4.ifconfig.co > ip.txt
 awk '{ print $0 "/32" }' < ip.txt > ipnew.txt
-export stuff=$ (cat ipnew.txt)
+export stuff=$(cat ipnew.txt)
 aws ec2 authorize-security-group-ingress --group-name NewGroup \
  --protocol tcp --port 22 --cidr $stuff
